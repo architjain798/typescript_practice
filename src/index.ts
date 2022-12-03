2d90720b3135d74b62d1138e5b61cbc13135e275
Archit Jain <77613361+architjain798@users.noreply.github.com>
2022-12-03 12:02:46 +0530

    Added the function to calculate the sum

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,15 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>`;
 
-function name(num: number) {
+function multpler(num: number) {
   return num * 2;
 }
-console.log(name(2));
+console.log(multpler(2));
+
+function sumOfArray(arr: number[]) {
+  return arr.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+}
+
+console.log(sumOfArray([1, 2, 3, 4]));
